05c34bebba7fb7b2f4465b6c716547ec2c55301f
redth <jondick@gmail.com>
2024-09-11 13:12:59 -0400

    iOS cleanup

--- Maui.ContentButton/ContentButtonHandler.ios.maccatalyst.cs
+++ Maui.ContentButton/ContentButtonHandler.ios.maccatalyst.cs
@@ -1,133 +1,104 @@
 ﻿#if IOS || MACCATALYST
+using System.Reflection;
 using Microsoft.Maui.Handlers;
 using Microsoft.Maui.Platform;
-using System.Reflection;
-using CoreGraphics;
 using UIKit;
 using MButton = UIKit.UIButton;
 
 namespace Maui.Extras
 {
-    public class ContentButtonWrapperView : WrapperView
-    {
-        public ContentButtonWrapperView(RectF frame) : base(frame)
-        {
-            
-        }
-        
-        PropertyInfo? wrapperViewCrossPlatformMeasureProperty;
-        PropertyInfo WrapperViewCrossPlatformMeasureProperty
-            => wrapperViewCrossPlatformMeasureProperty 
-                ??= typeof(WrapperView).GetProperty("CrossPlatformLayout",
-                    BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public)!;
+	public static class WrapperViewExtensions
+	{
+		static PropertyInfo? wrapperViewCrossPlatformMeasureProperty;
+		static PropertyInfo WrapperViewCrossPlatformMeasureProperty
+			=> wrapperViewCrossPlatformMeasureProperty
+				??= typeof(WrapperView).GetProperty("CrossPlatformLayout",
+					BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public)!;
 
-        public ICrossPlatformLayout CrossPlatformLayout
-        {
-            get => WrapperViewCrossPlatformMeasureProperty.GetValue(this) as ICrossPlatformLayout;
-            set => WrapperViewCrossPlatformMeasureProperty.SetValue(this, value);
-        }
+		public static ICrossPlatformLayout? GetCrossPlatformLayout(this WrapperView wrapperView)
+			=> WrapperViewCrossPlatformMeasureProperty.GetValue(wrapperView) as ICrossPlatformLayout;
 
-        public override CGSize SizeThatFits(CGSize size)
-        {
-            var baseSizeThatFits = base.SizeThatFits(size);
+		public static void SetCrossPlatformLayout(this WrapperView wrapperView, ICrossPlatformLayout value)
+			=> WrapperViewCrossPlatformMeasureProperty.SetValue(wrapperView, value);
+	}
 
-            var width = baseSizeThatFits.Width;
-            var height = baseSizeThatFits.Height;
+	public class ContentButtonMauiView : Microsoft.Maui.Platform.MauiView
+	{
+		public override void LayoutSubviews()
+		{
+			// This seems like extra work since MauiView does calculations then we do them again
+			base.LayoutSubviews();
 
-            if (!nfloat.IsInfinity(size.Width))
-                width = new nfloat(Math.Max(width.Value,  size.Width));
-            
-            if (!nfloat.IsInfinity(size.Height))
-                height = new nfloat(Math.Max(height.Value,  size.Height));
+			// Use the superview's bounds for the measurement constraints
+			var bounds = Superview.Bounds;
 
-            return new CGSize(width, height);
-        }
-    }
-    
-    
-    public class ContentWrapper : Microsoft.Maui.Platform.ContentView
-    {
-        public override CGSize SizeThatFits(CGSize size)
-        {
-            var baseSizeThatFits = base.SizeThatFits(size);
+			var widthConstraint = bounds.Width;
+			var heightConstraint = bounds.Height;
 
-            var width = baseSizeThatFits.Width;
-            var height = baseSizeThatFits.Height;
+			// Copied from the base implementation:
+			// If the SuperView is a MauiView (backing a cross-platform ContentView or Layout), then measurement
+			// has already happened via SizeThatFits and doesn't need to be repeated in LayoutSubviews. But we
+			// _do_ need LayoutSubviews to make a measurement pass if the parent is something else (for example,
+			// the window); there's no guarantee that SizeThatFits has been called in that case.
 
-            if (!nfloat.IsInfinity(size.Width))
-                width = new nfloat(Math.Max(width.Value,  size.Width));
-            
-            if (!nfloat.IsInfinity(size.Height))
-                height = new nfloat(Math.Max(height.Value,  size.Height));
+			if (!IsMeasureValid(widthConstraint, heightConstraint) && Superview is not MauiView)
+			{
+				CrossPlatformLayout?.CrossPlatformMeasure(widthConstraint, heightConstraint);
+				CacheMeasureConstraints(widthConstraint, heightConstraint);
+			}
 
-            return new CGSize(width, height);
-        }
-    }
-    
-    public partial class ContentButtonHandler : ViewHandler<IContentButton, MButton>
-    {
-        static readonly UIControlState[] ControlStates = { UIControlState.Normal, UIControlState.Highlighted, UIControlState.Disabled };
+			CrossPlatformLayout?.CrossPlatformArrange(bounds.ToRectangle());
+		}
+	}
 
-        public readonly static Thickness DefaultPadding = new Thickness(12, 7);
+	public partial class ContentButtonHandler : ViewHandler<IContentButton, MButton>
+	{
+		static readonly UIControlState[] ControlStates = { UIControlState.Normal, UIControlState.Highlighted, UIControlState.Disabled };
 
-        // Because we can't inherit from Button we use the container to handle
-        // Life cycle events and things like monitoring focus changed
-        public override bool NeedsContainer => true;
+		public readonly static Thickness DefaultPadding = new Thickness(12, 7);
 
-        
-        protected override UIButton CreatePlatformView()
-        {
-            var button = new UIButton(UIButtonType.System);
-            SetControlPropertiesFromProxy(button);
-            var contentSubview = new ContentWrapper
-            {
-                CrossPlatformLayout = VirtualView,
-                AutoresizingMask = UIViewAutoresizing.All,
-                Frame = button.Bounds
-            };
+		// Because we can't inherit from Button we use the container to handle
+		// Life cycle events and things like monitoring focus changed
+		public override bool NeedsContainer => true;
 
-            button.ClipsToBounds = true;
-            button.AddSubview(contentSubview);
-            return button;
-        }
 
-        readonly ButtonEventProxy _proxy = new ButtonEventProxy();
-        
-        protected override void SetupContainer()
-        {
-            if (PlatformView == null || ContainerView != null)
-                return;
+		protected override UIButton CreatePlatformView()
+		{
+			var button = new UIButton(UIButtonType.System);
+			SetControlPropertiesFromProxy(button);
 
-            var oldParent = (UIView?)PlatformView.Superview;
+			button.ClipsToBounds = true;
+			button.AddSubview(new ContentButtonMauiView
+			{
+				CrossPlatformLayout = VirtualView,
+			});
 
-            var oldIndex = oldParent?.IndexOfSubview(PlatformView);
-            PlatformView.RemoveFromSuperview();
+			return button;
+		}
 
-            ContainerView ??= new ContentButtonWrapperView(PlatformView.Bounds.ToRectangle())
-            {
-                CrossPlatformLayout = VirtualView
-            };
-            ContainerView.AddSubview(PlatformView);
+		readonly ButtonEventProxy _proxy = new ButtonEventProxy();
 
-            if (oldIndex is int idx && idx >= 0)
-                oldParent?.InsertSubview(ContainerView, idx);
-            else
-                oldParent?.AddSubview(ContainerView);
-        }
+		protected override void SetupContainer()
+		{
+			base.SetupContainer();
 
-        protected override void ConnectHandler(MButton platformView)
-        {
-            _proxy.Connect(VirtualView, platformView);
+			// We need to use reflection on this internal only property
+			(ContainerView as WrapperView)?.SetCrossPlatformLayout(VirtualView);
+		}
 
-            base.ConnectHandler(platformView);
-        }
+		protected override void ConnectHandler(MButton platformView)
+		{
+			_proxy.Connect(VirtualView, platformView);
 
-        protected override void DisconnectHandler(MButton platformView)
-        {
-            _proxy.Disconnect(platformView);
+			base.ConnectHandler(platformView);
+		}
 
-            base.DisconnectHandler(platformView);
-        }
+		protected override void DisconnectHandler(MButton platformView)
+		{
+			_proxy.Disconnect(platformView);
+
+			base.DisconnectHandler(platformView);
+		}
 
 #if MACCATALYST
 		public static void MapBackground(IContentButtonHandler handler, IContentButton button)
@@ -166,101 +137,101 @@ namespace Maui.Extras
 		}
 #endif
 
-        public static void MapStrokeColor(IContentButtonHandler handler, IButtonStroke buttonStroke)
-        {
-            handler.PlatformView?.UpdateStrokeColor(buttonStroke);
-        }
+		public static void MapStrokeColor(IContentButtonHandler handler, IButtonStroke buttonStroke)
+		{
+			handler.PlatformView?.UpdateStrokeColor(buttonStroke);
+		}
 
-        public static void MapStrokeThickness(IContentButtonHandler handler, IButtonStroke buttonStroke)
-        {
-            handler.PlatformView?.UpdateStrokeThickness(buttonStroke);
-        }
+		public static void MapStrokeThickness(IContentButtonHandler handler, IButtonStroke buttonStroke)
+		{
+			handler.PlatformView?.UpdateStrokeThickness(buttonStroke);
+		}
 
-        public static void MapCornerRadius(IContentButtonHandler handler, IButtonStroke buttonStroke)
-        {
-            handler.PlatformView?.UpdateCornerRadius(buttonStroke);
-        }
+		public static void MapCornerRadius(IContentButtonHandler handler, IButtonStroke buttonStroke)
+		{
+			handler.PlatformView?.UpdateCornerRadius(buttonStroke);
+		}
 
-        public static void MapPadding(IContentButtonHandler handler, IPadding padding)
-        {
-            handler.PlatformView?.UpdatePadding(padding.Padding, DefaultPadding);
-        }
+		public static void MapPadding(IContentButtonHandler handler, IPadding padding)
+		{
+			handler.PlatformView?.UpdatePadding(padding.Padding, DefaultPadding);
+		}
 
 
-        static void SetControlPropertiesFromProxy(UIButton platformView)
-        {
-            foreach (UIControlState uiControlState in ControlStates)
-            {
-                platformView.SetBackgroundImage(UIButton.Appearance.BackgroundImageForState(uiControlState), uiControlState);
-            }
-        }
+		static void SetControlPropertiesFromProxy(UIButton platformView)
+		{
+			foreach (UIControlState uiControlState in ControlStates)
+			{
+				platformView.SetBackgroundImage(UIButton.Appearance.BackgroundImageForState(uiControlState), uiControlState);
+			}
+		}
 
-        class ButtonEventProxy
-        {
-            WeakReference<IContentButton>? _virtualView;
+		class ButtonEventProxy
+		{
+			WeakReference<IContentButton>? _virtualView;
 
-            IContentButton? VirtualView => _virtualView is not null && _virtualView.TryGetTarget(out var v) ? v : null;
+			IContentButton? VirtualView => _virtualView is not null && _virtualView.TryGetTarget(out var v) ? v : null;
 
-            public void Connect(IContentButton virtualView, UIButton platformView)
-            {
-                _virtualView = new(virtualView);
+			public void Connect(IContentButton virtualView, UIButton platformView)
+			{
+				_virtualView = new(virtualView);
 
-                platformView.TouchUpInside += OnButtonTouchUpInside;
-                platformView.TouchUpOutside += OnButtonTouchUpOutside;
-                platformView.TouchDown += OnButtonTouchDown;
-            }
+				platformView.TouchUpInside += OnButtonTouchUpInside;
+				platformView.TouchUpOutside += OnButtonTouchUpOutside;
+				platformView.TouchDown += OnButtonTouchDown;
+			}
 
-            public void Disconnect(UIButton platformView)
-            {
-                _virtualView = null;
+			public void Disconnect(UIButton platformView)
+			{
+				_virtualView = null;
 
-                platformView.TouchUpInside -= OnButtonTouchUpInside;
-                platformView.TouchUpOutside -= OnButtonTouchUpOutside;
-                platformView.TouchDown -= OnButtonTouchDown;
-            }
+				platformView.TouchUpInside -= OnButtonTouchUpInside;
+				platformView.TouchUpOutside -= OnButtonTouchUpOutside;
+				platformView.TouchDown -= OnButtonTouchDown;
+			}
 
-            void OnButtonTouchUpInside(object? sender, EventArgs e)
-            {
-                if (VirtualView is IContentButton virtualView)
-                {
-                    virtualView.Released();
-                    virtualView.Clicked();
-                }
-            }
+			void OnButtonTouchUpInside(object? sender, EventArgs e)
+			{
+				if (VirtualView is IContentButton virtualView)
+				{
+					virtualView.Released();
+					virtualView.Clicked();
+				}
+			}
 
-            void OnButtonTouchUpOutside(object? sender, EventArgs e)
-            {
-                VirtualView?.Released();
-            }
+			void OnButtonTouchUpOutside(object? sender, EventArgs e)
+			{
+				VirtualView?.Released();
+			}
 
-            void OnButtonTouchDown(object? sender, EventArgs e)
-            {
-                VirtualView?.Pressed();
-            }
-        }
+			void OnButtonTouchDown(object? sender, EventArgs e)
+			{
+				VirtualView?.Pressed();
+			}
+		}
 
-        public static void MapContent(IContentButtonHandler handler, IContentButton view)
-        {
-            _ = handler.PlatformView ?? throw new InvalidOperationException($"{nameof(PlatformView)} should have been set by base class.");
-            _ = handler.VirtualView ?? throw new InvalidOperationException($"{nameof(VirtualView)} should have been set by base class.");
-            _ = handler.MauiContext ?? throw new InvalidOperationException($"{nameof(MauiContext)} should have been set by base class.");
+		public static void MapContent(IContentButtonHandler handler, IContentButton view)
+		{
+			_ = handler.PlatformView ?? throw new InvalidOperationException($"{nameof(PlatformView)} should have been set by base class.");
+			_ = handler.VirtualView ?? throw new InvalidOperationException($"{nameof(VirtualView)} should have been set by base class.");
+			_ = handler.MauiContext ?? throw new InvalidOperationException($"{nameof(MauiContext)} should have been set by base class.");
 
-            var contentSubview = handler.PlatformView.FindDescendantView<ContentWrapper>();
-            if (contentSubview is not null)
-            {
-                contentSubview.ClearSubviews();
+			var contentSubview = handler.PlatformView.FindDescendantView<ContentButtonMauiView>();
+			if (contentSubview is not null)
+			{
+				contentSubview.ClearSubviews();
 
-                if (handler.VirtualView.PresentedContent is IView presentedView)
-                {
-                    var inner = presentedView.ToPlatform(handler.MauiContext);
-                    inner.Frame = handler.PlatformView.Bounds;
-                    inner.AutoresizingMask = UIViewAutoresizing.All;
-                    contentSubview.AddSubview(inner);
-                }
-            }
-        }
+				if (handler.VirtualView.PresentedContent is IView presentedView)
+				{
+					var inner = presentedView.ToPlatform(handler.MauiContext);
+					contentSubview.AddSubview(inner);
+				}
 
-    }
+				contentSubview.SetNeedsLayout();
+			}
+		}
+
+	}
 }
 
 #endif
--- Maui.ContentButton/Platforms/MacCatalyst/PlatformClass1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Maui.ButtonContentView
-{
-    // All the code in this file is only included on Mac Catalyst.
-    public class PlatformClass1
-    {
-    }
-}
