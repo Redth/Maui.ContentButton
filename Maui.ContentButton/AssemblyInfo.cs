6a26e4b60cbf7de42a47ed03df8433dae565bda3
redth <jondick@gmail.com>
2024-09-12 11:36:13 -0400

    Add nicer namespace for xaml

---
+++ Maui.ContentButton/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿[assembly: XmlnsDefinition(MauiContentButton.Constants.XamlNamespace, MauiContentButton.Constants.MauiContentButtonNamespace)]
+
+[assembly: Microsoft.Maui.Controls.XmlnsPrefix(MauiContentButton.Constants.XamlNamespace, "contentbutton")]
+
+namespace MauiContentButton;
+
+static class Constants
+{
+	public const string XamlNamespace = "http://schemas.microsoft.com/dotnet/2024/maui/contentbutton";
+	public const string MauiContentButtonNamespace = $"{nameof(MauiContentButton)}";
+}
