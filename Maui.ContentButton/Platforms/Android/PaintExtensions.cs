e1481b4a9c8bca0ea5b9aeb0d565a8469b71c2e4
redth <jondick@gmail.com>
2024-10-08 12:47:32 -0400

    Fix reflection on ApplyTo proxy

--- Maui.ContentButton/Platforms/Android/PaintExtensions.cs
+++ Maui.ContentButton/Platforms/Android/PaintExtensions.cs
@@ -5,12 +5,39 @@ namespace Maui.ContentButton;
 
 internal static class PaintExtensions
 {
+    static Type? paintExtensionsType;
+    static MethodInfo? applyMethodInfo;
+
     internal static void ApplyTo(this Paint paint, GradientDrawable gradientDrawable, int height, int width)
     {
-        var methods = typeof(Microsoft.Maui.Graphics.PaintExtensions).GetMethod("ApplyTo", BindingFlags.Static | BindingFlags.NonPublic);
-            
-            
-        methods.Invoke(null, new object[] { paint, gradientDrawable, height, width });
+        if (applyMethodInfo is null)
+        {
+            paintExtensionsType ??= typeof(Microsoft.Maui.Graphics.PaintExtensions);
+
+            var methods = paintExtensionsType.GetMethods(BindingFlags.Static | BindingFlags.NonPublic);
+
+            foreach (var m in methods)
+            {
+                if (m.Name != "ApplyTo")
+                    continue;
+
+                var p = m.GetParameters();
+                if (p.Length != 4)
+                    continue;
+
+                if (p[0].ParameterType == typeof(Paint) && p[1].ParameterType == typeof(GradientDrawable)
+                    && p[2].ParameterType == typeof(int) && p[3].ParameterType == typeof(int))
+                {
+                    applyMethodInfo = m;
+                    break;
+                }
+            }
+        }
+
+        if (applyMethodInfo != null)
+        {
+			applyMethodInfo.Invoke(null, new object[] { paint, gradientDrawable, height, width });
+		}
     }
 
 }
