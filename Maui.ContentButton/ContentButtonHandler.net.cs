4e9be46149348d7a4d04412ff2b50a25b083e788
redth <jondick@gmail.com>
2024-12-06 15:34:40 -0500

    No need to throw

--- Maui.ContentButton/ContentButtonHandler.net.cs
+++ Maui.ContentButton/ContentButtonHandler.net.cs
@@ -5,39 +5,32 @@ namespace MauiContentButton;
 #if !MACCATALYST && !IOS && !ANDROID && !WINDOWS
 public partial class ContentButtonHandler : ViewHandler<IContentButton, object>
 {
-    protected override object CreatePlatformView()
-    {
-        throw new NotImplementedException();
-    }
+	protected override object CreatePlatformView()
+	{
+	}
 	
-    public static void MapContent(IContentButtonHandler handler, IContentButton view)
-    {
-        throw new NotImplementedException();
-    }
+	public static void MapContent(IContentButtonHandler handler, IContentButton view)
+	{
+	}
 
-    public static void MapBackground(IContentButtonHandler handler, IContentButton view)
-    {
-        throw new NotImplementedException();
-    }
+	public static void MapBackground(IContentButtonHandler handler, IContentButton view)
+	{
+	}
 
-    public static void MapStrokeColor(IContentButtonHandler handler, IButtonStroke buttonStroke)
-    {
-        throw new NotImplementedException();
-    }
+	public static void MapStrokeColor(IContentButtonHandler handler, IButtonStroke buttonStroke)
+	{
+	}
 
-    public static void MapStrokeThickness(IContentButtonHandler handler, IButtonStroke buttonStroke)
-    {
-        throw new NotImplementedException();
-    }
+	public static void MapStrokeThickness(IContentButtonHandler handler, IButtonStroke buttonStroke)
+	{
+	}
 
-    public static void MapCornerRadius(IContentButtonHandler handler, IButtonStroke buttonStroke)
-    {
-        throw new NotImplementedException();
-    }
+	public static void MapCornerRadius(IContentButtonHandler handler, IButtonStroke buttonStroke)
+	{
+	}
 
-    public static void MapPadding(IContentButtonHandler handler, IPadding padding)
-    {
-        throw new NotImplementedException();
-    }
+	public static void MapPadding(IContentButtonHandler handler, IPadding padding)
+	{
+	}
 }
 #endif
