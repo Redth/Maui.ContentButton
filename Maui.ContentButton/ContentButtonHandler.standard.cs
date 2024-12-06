90d708b6b53549a7005b3ca44fb8eccf20767baf
redth <jondick@gmail.com>
2024-12-06 15:49:46 -0500

    Switch to ifdefs for standard code

--- Maui.ContentButton/ContentButtonHandler.standard.cs
+++ Maui.ContentButton/ContentButtonHandler.standard.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Maui.Handlers;
+﻿#if !IOS && !MACCATALYST && !ANDROID && !WINDOWS
+using Microsoft.Maui.Handlers;
 using PlatformButtonView = object;
 
 namespace MauiContentButton;
@@ -6,15 +7,16 @@ namespace MauiContentButton;
 // All the code in this file is included in all platforms.
 public partial class ContentButtonHandler : ViewHandler<IContentButton, PlatformButtonView>
 {
-    protected override PlatformButtonView CreatePlatformView() => new();
+	protected override PlatformButtonView CreatePlatformView() => new();
 
-    private static void MapPadding(IContentButtonHandler handler, IContentButton view) { }
+	private static void MapPadding(IContentButtonHandler handler, IContentButton view) { }
 
-    private static void MapStrokeThickness(IContentButtonHandler handler, IContentButton view) { }
+	private static void MapStrokeThickness(IContentButtonHandler handler, IContentButton view) { }
 
-    private static void MapStrokeColor(IContentButtonHandler handler, IContentButton view) { }
+	private static void MapStrokeColor(IContentButtonHandler handler, IContentButton view) { }
 
-    private static void MapCornerRadius(IContentButtonHandler handler, IContentButton view) { }
+	private static void MapCornerRadius(IContentButtonHandler handler, IContentButton view) { }
 
-    private static void MapContent(IContentButtonHandler handler, IContentButton view) { }
+	private static void MapContent(IContentButtonHandler handler, IContentButton view) { }
 }
+#endif
